108b8925ab675e0c6c991625b300e57779d45be1
Javad Afshar <mjafshar@gmail.com>
2014-06-28 16:31:11 -0500

    added friendships relationships and models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 javad = User.create(name: 'Javad', email: 'javad@javad.com', password: 'javad', password_confirmation: 'javad', has_car?: false)
 harry = User.create(name: 'Harry', email: 'harry@harry.com', password: 'harry', password_confirmation: 'harry', has_car?: true)
+javad.friends << harry
+harry.friends << javad
 
 dbc = Ride.create(name: 'DBC')
 
