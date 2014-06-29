9306129a92ba09377cbfea5b16b5bc3787a71d5d
Harry Wang <hgw2101@gmail.com>
2014-06-29 05:32:02 -0500

    add maps.js file to construct map on the ide show page

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,44 @@
+var directionsShow;
+var directionsTool = new google.maps.DirectionsService();
+var map;
+
+function initializeMap() {
+  directionsShow = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var mapOptions = {
+    zoom:7,
+    center: chicago
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  directionsShow.setMap(map);
+}
+
+function calcPath() {
+  // var start = document.getElementById('start').value;
+  // var end = document.getElementById('end').value;
+  var start = '41.8822784,-87.6294101'
+  var end = '42.1168454,-85.6345205'
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsTool.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsShow.setDirections(response);
+    }
+  });
+}
+
+// google.maps.event.addDomListener(window, 'load', initializeMap);
+
+$(document).ready(function() {
+  initializeMap();
+  calcPath();
+});
+
+
+$(document).on('page:load', function() {
+  initializeMap();
+  calcPath();
+});
