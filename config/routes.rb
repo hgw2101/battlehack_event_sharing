094e21ced7e487d28416d48dd83f1dc9b70e2683
Javad Afshar <mjafshar@gmail.com>
2014-06-29 09:36:40 -0500

    Added new route to confirm payment.

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   post '/users/user_rides/:id/rider_approve', to: 'user_rides#rider_approve', as: 'rider_approve'
   post '/rides/user_rides/:id/driver_approve', to: 'user_rides#driver_approve', as: 'driver_approve'
   post '/rides/user_rides/:ride_id', to: 'user_rides#create', as: 'user_ride_create'
-
+  post '/rides/user_rides/:id/payment', to: 'user_rides#payment', as: 'payment'
 end
