d3534d3e9775feef413f99712ef7488a3851cb1b
Harry Wang <hgw2101@gmail.com>
2014-06-29 01:44:54 -0500

    change type column in location_records table to description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628205442) do
+ActiveRecord::Schema.define(version: 20140629063638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20140628205442) do
     t.integer  "locatable_id"
     t.string   "locatable_type"
     t.spatial  "location",       limit: {:srid=>4326, :type=>"point", :geographic=>true}
-    t.string   "type"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
